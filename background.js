b92c01b538b458c8d1f14b16762caa1717018072
eetann <hideharu092@gmail.com>
2020-08-10 10:49:39 +0900

    for文内条件式の更新、Alt押しで新しいタブで開く

--- background.js
+++ background.js
@@ -1,31 +1,31 @@
-chrome.omnibox.onInputChanged.addListener(
-  function (text, suggest) {
-    var fuz = "";
-    for (var i = 0; i < text.length; i++) {
-      fuz += text.charAt(i) + ".*";
-    }
-    var re = new RegExp(fuz, "i");
-    chrome.bookmarks.search({}, function (bookmarkItems) {
-      var sugs = [];
-      for (var i = 0; i < bookmarkItems.length; i++) {
-        var item = bookmarkItems[i];
-        if ((item.url) && (re.test(item.title))) {
-          sugs.push({content: item.url, description: item.title});
-        }
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+  var fuz = "";
+  for (var i = 0; i < text.length; i++) {
+    fuz += text.charAt(i) + ".*";
+  }
+  var re = new RegExp(fuz, "i");
+  chrome.bookmarks.search({}, (bookmarkItems) => {
+    var sugs = [];
+    for (var i = 0; i < bookmarkItems.length && sugs.length < 7; i++) {
+      var item = bookmarkItems[i];
+      if ((item.url) && (re.test(item.title))) {
+        sugs.push({content: item.url, description: item.title});
       }
-      suggest(sugs);
-    });
-  }
-);
-
-chrome.omnibox.onInputEntered.addListener(
-  function (text) {
-    navigate(text);
-  }
-);
-
-function navigate(url) {
-  chrome.tabs.getSelected(null, function (tab) {
-    chrome.tabs.update(tab.id, {url: url});
+    }
+    suggest(sugs);
   });
-}
+});
+
+chrome.omnibox.onInputEntered.addListener((text, disposition) => {
+  switch (disposition) {
+    case "currentTab":
+      chrome.tabs.update({url: text});
+      break;
+    case "newForegroundTab":
+      chrome.tabs.create({url: text});
+      break;
+    case "newBackgroundTab":
+      chrome.tabs.create({url: text, active: false});
+      break;
+  }
+});
