3b64b5f6e46c7325fac60ee6ce690f511e9c7587
eetann <hideharu092@gmail.com>
2020-08-11 14:35:27 +0900

    chrome.storage.localを使うように変更

--- background.js
+++ background.js
@@ -1,22 +1,45 @@
+chrome.omnibox.setDefaultSuggestion({
+  description: "Please type a few words the title of the bookmark."
+});
+
+chrome.omnibox.onInputStarted.addListener(() => {
+  chrome.bookmarks.search({}, (bookmarkItems) => {
+    if (bookmarkItems) {
+      var bookmarks = [];
+      for (var i = 0; i < bookmarkItems.length; i++) {
+        var item = bookmarkItems[i];
+        if (item.url) {
+          bookmarks.push({content: item.url, description: item.title});
+        }
+      }
+      chrome.storage.local.set({"mrsagasu": bookmarks});
+    }
+  })
+});
+
 chrome.omnibox.onInputChanged.addListener((text, suggest) => {
   var fuz = "";
   for (var i = 0; i < text.length; i++) {
     fuz += text.charAt(i) + ".*";
   }
   var re = new RegExp(fuz, "i");
-  chrome.bookmarks.search({}, (bookmarkItems) => {
-    var sugs = [];
-    for (var i = 0; i < bookmarkItems.length && sugs.length < 7; i++) {
-      var item = bookmarkItems[i];
-      if ((item.url) && (re.test(item.title))) {
-        sugs.push({content: item.url, description: item.title});
+  chrome.storage.local.get("mrsagasu", (value) => {
+    var bookmarks = value.mrsagasu;
+    if (bookmarks) {
+      var sugs = [];
+      for (var i = 0; i < bookmarks.length && sugs.length < 7; i++) {
+        var item = bookmarks[i];
+        if (re.test(item.description)) {
+          sugs.push(item);
+        }
       }
+      suggest(sugs);
     }
-    suggest(sugs);
   });
 });
 
 chrome.omnibox.onInputEntered.addListener((text, disposition) => {
+  chrome.storage.local.remove("mrsagasu");
   switch (disposition) {
     case "currentTab":
       chrome.tabs.update({url: text});
@@ -28,4 +51,9 @@ chrome.omnibox.onInputEntered.addListener((text, disposition) => {
       chrome.tabs.create({url: text, active: false});
       break;
   }
+});
+
+
+chrome.omnibox.onInputCancelled.addListener(() => {
+  chrome.storage.local.remove("mrsagasu");
 });
