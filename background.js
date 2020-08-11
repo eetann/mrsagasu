024fd980476f66ef7686cf2ca2db93162464c3df
eetann <hideharu092@gmail.com>
2020-08-11 17:14:27 +0900

    リファクタリング

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-chrome.omnibox.setDefaultSuggestion({
-  description: "Please type a few words the title of the bookmark."
-});
-
 function update_bookmarks() {
   chrome.bookmarks.search({}, (bookmarkItems) => {
     if (bookmarkItems) {
@@ -22,15 +18,19 @@ chrome.bookmarks.onChanged.addListener(update_bookmarks);
 chrome.bookmarks.onRemoved.addListener(update_bookmarks);
 chrome.bookmarks.onCreated.addListener(update_bookmarks);
 
+chrome.omnibox.setDefaultSuggestion({
+  description: "Please type a few words the title of the bookmark."
+});
+
 chrome.omnibox.onInputChanged.addListener((text, suggest) => {
-  var fuz = "";
-  for (var i = 0; i < text.length; i++) {
-    fuz += text.charAt(i) + ".*?";
-  }
-  var re = new RegExp(fuz, "i");
   chrome.storage.local.get("mrsagasu", (value) => {
     var bookmarks = value.mrsagasu;
     if (bookmarks) {
+      var fuz = "";
+      for (var i = 0; i < text.length; i++) {
+        fuz += text.charAt(i) + ".*?";
+      }
+      var re = new RegExp(fuz, "i");
       var sugs = bookmarks.filter(value => re.test(value.description));
       sugs.sort((a, b) => {
         return re.exec(a.description)[0].length - re.exec(b.description)[0].length;
