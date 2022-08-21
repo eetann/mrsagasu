af2128b2df5f2df2904c573d9672640789114872
eetann <hideharu092@gmail.com>
2022-08-21 20:28:33 +0900

    change manifest #2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import manifest from "./src/manifest";
+import { crx } from "@crxjs/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), crx({ manifest })],
+});
