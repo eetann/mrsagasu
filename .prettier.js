bf5c1227fdb8c78eb0e412f03c08764419ce665d
eetann <hideharu092@gmail.com>
2022-08-21 20:12:50 +0900

    add eslint and prettier

---
+++ .prettier.js
@@ -0,0 +1,6 @@
+module.exports = {
+  importOrder: ["^@core/(.*)$", "^@server/(.*)$", "^@ui/(.*)$", "^[./]"],
+  importOrderCaseInsensitive: true,
+  importOrderSortSpecifiers: true,
+  importOrderSeparation: true,
+}
