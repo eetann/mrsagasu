bd6929e79b2c3e0219058f1331b259ba3af5ec0d
eetann <hideharu092@gmail.com>
2022-09-05 22:18:50 +0900

    WIP: nickname form #10

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,41 @@ const App = (): JSX.Element => {
     });
   }, []);
 
+  const onSubmit = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      const regexp = new RegExp(`#${query.current?.value}(\\s|$)`);
+      const bookmarks = allBookmarks.filter((item) => regexp.test(item.title));
+      for (const bookmark of bookmarks) {
+        chrome.tabs.create({ url: bookmark.url });
+      }
+    }
+  };
+
   return (
-    <div>
-      <input
-        autoFocus={true}
-        type="text"
-        ref={query}
-        onKeyPress={(e) => {
-          if (e.key === "Enter") {
-            const regexp = new RegExp(`#${query.current?.value}(\\s|$)`);
-            const bookmarks = allBookmarks.filter((item) =>
-              regexp.test(item.title)
-            );
-            for (const bookmark of bookmarks) {
-              chrome.tabs.create({ url: bookmark.url });
-            }
-          }
-        }}
-      />
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Bookmark Nickname?</span>
+          </label>
+          <label className="input-group input-group-md">
+            <span className="px-2 text-xl">#</span>
+            <input
+              placeholder="Type here"
+              className="input input-bordered input-md w-full max-w-xs px-2 text-xl"
+              autoFocus={true}
+              type="text"
+              ref={query}
+              onKeyPress={onSubmit}
+            />
+          </label>
+        </div>
+        <div className="card-actions justify-end">
+          <button className="btn btn-ghost" onClick={onSubmit}>
+            <kbd className="kbd kbd-sm">Enter</kbd>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
