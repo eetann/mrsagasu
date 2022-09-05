e631d0aaf8c60ba7ccfa9ff033b9cb455671ef07
eetann <hideharu092@gmail.com>
2022-09-05 21:37:03 +0900

    fix prettier settings #10

--- .prettier.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  importOrder: ["^@core/(.*)$", "^@server/(.*)$", "^@ui/(.*)$", "^[./]"],
-  importOrderCaseInsensitive: true,
-  importOrderSortSpecifiers: true,
-  importOrderSeparation: true,
-}
