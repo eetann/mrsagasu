2516c3c9cd536d6523cfd11a2cfcbd1dcdf71f85
eetann <hideharu092@gmail.com>
2022-09-05 20:19:03 +0900

    add popup form #10

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-function App() {
+import { useEffect, useRef, useState } from "react";
+
+const App = (): JSX.Element => {
+  const [allBookmarks, setAllBookMarks] = useState<
+    chrome.bookmarks.BookmarkTreeNode[]
+  >([]);
+  const query = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    chrome.bookmarks.search({}, (bookmarkItems) => {
+      setAllBookMarks(bookmarkItems.filter((item) => "url" in item));
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Vite + React</h1>
+    <div>
+      <input
+        autoFocus={true}
+        type="text"
+        ref={query}
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            const regexp = new RegExp(`#${query.current?.value}(\\s|$)`);
+            const bookmarks = allBookmarks.filter((item) =>
+              regexp.test(item.title)
+            );
+            for (const bookmark of bookmarks) {
+              chrome.tabs.create({ url: bookmark.url });
+            }
+          }
+        }}
+      />
     </div>
   );
-}
+};
 
 export default App;
